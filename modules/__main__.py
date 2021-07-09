61404872d734e1c270e3d7b664507ea253c65046
crekha1 <80155306+crekha1@users.noreply.github.com>
2021-07-09 22:47:24 +0530

    Update __main__.py

--- modules/__main__.py
+++ modules/__main__.py
@@ -1,11 +1,11 @@
 import requests
 from pyrogram import Client as Bot
 
-from utils.config import API_HASH
-from utils.config import API_ID
-from utils.config import BG_IMAGE
-from utils.config import BOT_TOKEN
-from utils.services.callsmusic import run
+from env.config import API_HASH
+from env.config import API_ID
+from env.config import BG_IMAGE
+from env.config import BOT_TOKEN
+from env.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
