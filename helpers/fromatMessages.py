005e5bf2fb035d9ad7a4476805490707af2c5af0
insidejob <84038103+insidescode@users.noreply.github.com>
2021-07-09 21:06:42 +0530

    Update fromatMessages.py

--- helpers/fromatMessages.py
+++ helpers/fromatMessages.py
@@ -10,17 +10,18 @@ def getMessage(message, action):
         ALLOWED_CHAT_TYPES = config.get("ALLOWED_CHAT_TYPES")
 
         if action == "private-chat":
-            send_message = f"**Hi 🎵 {message.chat.first_name if hasattr(message.chat, 'first_name') else 'User'}**"
+            send_message = f"**Hey 👋 {message.chat.first_name if hasattr(message.chat, 'first_name') else 'User'}**"
             send_message = send_message + \
-                f"\n\n**[Voice Chat Music Player]({config.get('BOT_URL')})** is a [SkTechHub Product]({config.get('PARENT_URL')})."
+                f"\n\n**Zer0Byte Music 3.0 Beta]({config.get('BOT_URL')})** is a [Zer0Byte Project](t.me/Zer0ByteOfficial)."
             send_message = send_message + \
-                f"\n__It is designed to play, as simple as possible, music in your groups through the **new voice chats** introduced by Telegram.__"
+                f"\nZer0Byte 2.0 is a project designed for play music in your **group's voice Chat**, as simple as possible"
+
             send_message = send_message + \
-                f"\n\n**So why wait 🌀 add the bot to a group and get started 🎧**\n\n**Source Code :** [Repository]({config.get('GITHUB_REPO')})"
+                f"\n\n**This is not fully developed its still on testing!**\n\n**Source Code :** [Repository](https://telegra.ph/file/6d661cc458396796f4692.jpg)"
             return send_message, getReplyKeyBoard(message, action)
 
         elif action == "help-msg":
-            helpMessage = f"**VoiceChat Music Player**\n**Source Code :** [Repository]({config.get('GITHUB_REPO')})"
+            helpMessage = f"**Zer0Byte Music beta**\n**Source Code :** [Repository](https://telegra.ph/file/6d661cc458396796f4692.jpg)"
             helpMessage = helpMessage + \
                 f"\n\n• **/play song name/song url : ** __Start a song / add to queue.__"
             helpMessage = helpMessage + f"\n• **/skip : ** __Skip to the next song in queue.__"
@@ -37,15 +38,15 @@ def getMessage(message, action):
                 f"\n• **/adminmode on|off : ** __Turning this on makes the bot actions available only to bot admins.__"
             helpMessage = helpMessage + \
                 f"\n• **/loop [2-5]|off : ** __Loop the playback [x] times(x is between 2-5) / Turn off the loop playback.__"
-            helpMessage = helpMessage + f"\n\n**__For any issues contact @voicechatsupport__**"
+            helpMessage = helpMessage + f"\n\n**__For any issues contact @Zer0ByteSupport**"
             return helpMessage, getReplyKeyBoard(message, action)
 
         elif action == "chat-not-allowed":
-            send_message = f"**😖 Sorry but this chat is not yet allowed to access the service. You can always check the demo in [Support Group]({config.get('SUPPORT_GROUP')}).**"
+            send_message = f"**Sorry but this chat is not yet allowed to access the service. You can always check the demo in [Support Group](https://t.me/Zer0ByteSupport).**"
             send_message = send_message + \
-                f"\n\n**Why ❓**\n- __Due to high usage we have restrcited the usage of the bot in just our [Support Group]({config.get('SUPPORT_GROUP')}) __"
+                f"\n\n**Why ❓**\n- __Due to high usage we have restrcited the usage of the bot in just our [Support Group](https://t.me/Zer0ByteSupport) __"
             send_message = send_message + \
-                f"\n- __Join the [Support Group]({config.get('SUPPORT_GROUP')}) to access the bot or deploy your own bot __ **Source Code :** [Github]({config.get('GITHUB_REPO')})"
+                f"\n- __Join the [Support Group](https://t.me/Zer0ByteSupport) to access the bot or deploy your own bot __ **Source Code :** [Github](https://telegra.ph/file/6d661cc458396796f4692.jpg)"
 
             return send_message, getReplyKeyBoard(message, action)
 
@@ -68,14 +69,14 @@ def getReplyKeyBoard(message, action):
                 [
                     [
                         InlineKeyboardButton(
-                            "➕ Add the bot to Group ➕", url=f"{config.get('BOT_URL')}?startgroup=bot"),
+                            "➕ Add Zer0Byte Music Beta ➕", url=f"{config.get('BOT_URL')}?startgroup=bot"),
                     ],
                     [
                         InlineKeyboardButton(
-                            "👥 Support Group", url=f"{config.get('SUPPORT_GROUP')}"),
+                            "👥 Support Group", url=f"https://t.me/Zer0ByteOfficial"),
 
                         InlineKeyboardButton(
-                            "📔 Source Code", url=f"{config.get('GITHUB_REPO')}"),
+                            "🔔 Updates", url=f"https://t.me/Zer0ByteSupport"),
                     ],
 
                 ]
@@ -86,11 +87,11 @@ def getReplyKeyBoard(message, action):
                 [
                     [
                         InlineKeyboardButton(
-                            "🏁 Use In Demo Group", url=f"{config.get('SUPPORT_GROUP')}"),
+                            "🏁 Use In Demo Group", url=f"https://t.me/Friends_Chatting_Grp"),
                     ],
                     [
                         InlineKeyboardButton(
-                            "📔 Source Code", url=f"{config.get('GITHUB_REPO')}"),
+                            "🗃 Source Code", url=f"https://telegra.ph/file/6d661cc458396796f4692.jpg"),
 
                     ],
 
